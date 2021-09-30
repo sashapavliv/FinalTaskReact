9c154934adefbba22beb69a0b2e60838446d32fc
sashapavliv <sanyadas@ukr.net>
2021-09-30 22:20:20 +0300

    fix some styles

--- src/modals/DeleteUserModal/DeleteUserModal.tsx
+++ src/modals/DeleteUserModal/DeleteUserModal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Modal} from "antd";
+import './deleteModalStyle.scss';
 
 const DeleteUserModal = ({open,submit,handleCancel}:any) => {
     return (
-        <div>
-            <Modal title="Basic Modal" visible={open} onOk={submit} onCancel={handleCancel}>
+        <div className={'delete-modal'}>
+            <Modal className={'title-modal'} title="Delete user" visible={open} onOk={submit} onCancel={handleCancel}>
                 <p>Are you sure you want to delete this user?</p>
             </Modal>
         </div>
